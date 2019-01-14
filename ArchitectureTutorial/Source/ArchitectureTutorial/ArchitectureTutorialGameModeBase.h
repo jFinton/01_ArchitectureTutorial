eb305c58b2e7b538f1e5ac23c5d7b54cfdb0898a
GenOnox <grandwrath86@gmail.com>
2019-01-14 05:17:02 -0500

    Establishing gitignore and creating project

---
+++ ArchitectureTutorial/Source/ArchitectureTutorial/ArchitectureTutorialGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArchitectureTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHITECTURETUTORIAL_API AArchitectureTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
